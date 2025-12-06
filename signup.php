7ec76534fb8073647eba607129625af358fb3817
reemsaifi <reemsaifi478@gmail.com>
2025-12-06 18:11:58 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,7 +6,8 @@ $name     = $_POST['name'];
 $phone    = $_POST['phone'];
 $email    = $_POST['email'];
 $password = $_POST['password'];
-$type     = $_POST['role']; // "student" أو "renter"
+$type     = $_POST['type'];
+$role     =$_POST['role']; // "student" أو "renter"
 
 $hashed = password_hash($password, PASSWORD_DEFAULT);
 
@@ -25,15 +26,15 @@ if($result_renter->num_rows > 0 || $result_student->num_rows > 0){
 }
 
 // تسجيل البيانات حسب النوع
-if($type == "renter"){
-    $sql = "INSERT INTO renter (name, phone, email, password) VALUES ('$name','$phone','$email','$hashed')";
+if($role == "renter"){
+    $sql = "INSERT INTO renter (name, phone, email, password,gender) VALUES ('$name','$phone','$email','$hashed','$type')";
     if($conn->query($sql) === TRUE){
         $_SESSION['renter_id'] = $conn->insert_id;
         header("Location: myposts.html"); // ضع هنا صفحة لوحة المؤجر
         exit();
     }
 }else{
-    $sql = "INSERT INTO student (name, phone, email, password) VALUES ('$name','$phone','$email','$hashed')";
+    $sql = "INSERT INTO student (name, phone, email, password,gender) VALUES ('$name','$phone','$email','$hashed','$type')";
     if($conn->query($sql) === TRUE){
         $_SESSION['student_id'] = $conn->insert_id;
         header("Location: index.html"); // ضع هنا الصفحة الرئيسية للطالب
